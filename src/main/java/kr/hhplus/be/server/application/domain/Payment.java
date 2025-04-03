4ef255810970ca5ebd0f217dfe79e15d9f612f33
hakbinMac <gkrqls8409@gmail.com>
2025-04-04 04:07:59 +0900

    [2주차][심화] 결제 요청 controller 단위 테스트 작성 #5

--- src/main/java/kr/hhplus/be/server/application/domain/Payment.java
+++ src/main/java/kr/hhplus/be/server/application/domain/Payment.java
@@ -5,6 +5,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "결제")
 public class Payment {
 
+    public Payment(String bookingId) {
+        this.bookingId = bookingId;
+    }
+
     @Schema(description = "예약id", example = "1")
     private String bookingId;
 
---
+++ src/test/java/kr/hhplus/be/server/application/controller/PaymentControllerTest.java
@@ -0,0 +1,51 @@
+package kr.hhplus.be.server.application.controller;
+
+import kr.hhplus.be.server.application.common.ResponseMessage;
+import kr.hhplus.be.server.application.domain.Payment;
+import kr.hhplus.be.server.application.response.PaymentResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class PaymentControllerTest {
+
+    @DisplayName("결제 요청 테스트")
+    @Test
+    void paymentTest(){
+
+        //given
+        PaymentController controller = new PaymentController();
+        Payment payment = new Payment("1");
+
+        // When
+        ResponseMessage<PaymentResponse> response = controller.payment(payment);
+
+        //then
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        assertEquals("결제가 완료됐습니다.", response.getMessage());
+
+    }
+
+    @DisplayName("payment 파라미터 중 bookId가 존재하지 않으면 결제할 수 없다.")
+    @Test
+    void paymentWithBookIdTest(){
+
+        //given
+        PaymentController controller = new PaymentController();
+        Payment payment = new Payment("");
+
+        // When
+        ResponseMessage<PaymentResponse> response = controller.payment(payment);
+
+        //then
+        assertNotNull(response);
+        assertNotEquals(200, response.getStatus());
+        assertEquals("결제에 실패했습니다.", response.getMessage());
+
+    }
+
+
+}
