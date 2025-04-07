e561cb1bf57f5e8526454f7e07f2b28f567db0f8
hakbinMac <gkrqls8409@gmail.com>
2025-04-08 00:52:58 +0900

    [3주차][기본] fix : @RestControllerAdvice 사용으로 인한 swagger 오류
    @RestControllerAdvice 주석

--- src/main/java/kr/hhplus/be/server/src/common/exception/ControllerExceptionHandler.java
+++ src/main/java/kr/hhplus/be/server/src/common/exception/ControllerExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+//@RestControllerAdvice
 @RequiredArgsConstructor
 public class ControllerExceptionHandler {
 
