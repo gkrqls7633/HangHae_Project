d324f3d4f6edb76b707b52f7b0818a3c93e6119d
hakbinMac <gkrqls8409@gmail.com>
2025-04-10 02:31:24 +0900

    [3주차][기본] PayRequest 결제 요청 DTO sample value 작성 #20

--- src/main/java/kr/hhplus/be/server/src/interfaces/payment/PaymentRequest.java
+++ src/main/java/kr/hhplus/be/server/src/interfaces/payment/PaymentRequest.java
@@ -1,14 +1,19 @@
 package kr.hhplus.be.server.src.interfaces.payment;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+@Schema(description = "결제 요청 Request DTO")
 @Getter
 public class PaymentRequest {
 
+    @Schema(description = "예약 id", example = "1", required = true)
     private Long bookingId;
 
+    @Schema(description = "유저 id", example = "123", required = true)
     private Long userId;
 
-    private Long paymentPrice;
+//    @Schema(description = "paymentPrice", example = "1", required = true)
+//    private Long paymentPrice;
 
 }
