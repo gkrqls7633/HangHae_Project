a203870a416f836026223d62ac7db30dcf3d1367
hakbinMac <gkrqls8409@gmail.com>
2025-04-16 01:08:26 +0900

    [4과제][기본] BookingSeat 통합테스트 환경 구성
    - beforeEach 구성

--- src/test/java/kr/hhplus/be/server/src/service/integration/BookingSeatServiceIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/src/service/integration/BookingSeatServiceIntegrationTest.java
@@ -1,4 +1,32 @@
 package kr.hhplus.be.server.src.service.integration;
 
+import kr.hhplus.be.server.src.domain.model.Booking;
+import kr.hhplus.be.server.src.interfaces.booking.BookingRequest;
+import kr.hhplus.be.server.src.service.testTransactionHelper.BookingTransactionHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 class BookingSeatServiceIntegrationTest {
+
+    @Autowired
+    private BookingTransactionHelper bookingTransactionHelper;
+
+    private BookingRequest bookingRequest;
+
+    @BeforeEach
+    void setup() {
+        bookingRequest = bookingTransactionHelper.setupTestData();
+    }
+
+    @DisplayName("좌석 예약 요청 시 콘서트id, 좌석num, userId 기반으로 해당 좌석 점유 및 예약상태가 된다.")
+    @Test
+    void bookingSeatIntegrationTest() {
+
+    }
 }
