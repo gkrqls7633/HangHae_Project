3bbe9da49e2501b3723b1c954c7a6fddef34ed31
hakbinMac <gkrqls8409@gmail.com>
2025-04-17 03:07:32 +0900

    [4주차][기본] Payment Service unit 테스트 작성 #35

--- src/main/java/kr/hhplus/be/server/src/service/PaymentService.java
+++ src/main/java/kr/hhplus/be/server/src/service/PaymentService.java
@@ -2,20 +2,17 @@ package kr.hhplus.be.server.src.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import kr.hhplus.be.server.src.common.ResponseMessage;
-import kr.hhplus.be.server.src.domain.model.Booking;
-import kr.hhplus.be.server.src.domain.model.Concert;
-import kr.hhplus.be.server.src.domain.model.Payment;
-import kr.hhplus.be.server.src.domain.model.Point;
+import kr.hhplus.be.server.src.domain.model.*;
 import kr.hhplus.be.server.src.domain.model.enums.PaymentStatus;
-import kr.hhplus.be.server.src.domain.repository.BookingRepository;
-import kr.hhplus.be.server.src.domain.repository.ConcertRepository;
-import kr.hhplus.be.server.src.domain.repository.PaymentRepository;
-import kr.hhplus.be.server.src.domain.repository.PointRepository;
+import kr.hhplus.be.server.src.domain.model.enums.SeatStatus;
+import kr.hhplus.be.server.src.domain.repository.*;
 import kr.hhplus.be.server.src.interfaces.payment.PaymentRequest;
 import kr.hhplus.be.server.src.interfaces.payment.PaymentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 
 @RequiredArgsConstructor
@@ -26,6 +23,7 @@ public class PaymentService {
     private final PointRepository pointRepository;
     private final ConcertRepository concertRepository;
     private final BookingRepository bookingRepository;
+    private final SeatRepository seatRepository;
 
     @Transactional
     public ResponseMessage<PaymentResponse> processPayment(PaymentRequest paymentRequest) {
@@ -42,11 +40,20 @@ public class PaymentService {
                 .build();
 
         /*
-        2. bookingId -> 결제할 콘서트 정보 조회 (가격)
-         - 결제할 콘서트의 예약 여부 체크 (좌석 점유 되어있는지)
-         - Payment -> Booking
+        2. 결제 요청 유저와 예약된 유저 동일한지 체크
          */
         if (!paymentDomain.isBookingCheck(booking)) {
+            throw new IllegalArgumentException("유저 정보가 일치하지 않습니다.");
+        }
+
+        /*
+        3.. 결제할 콘서트의 예약 여부 체크 (좌석 점유 되어있는지)
+         - Booking -> Seat -> seatStatus
+         */
+        Seat seat = seatRepository.findById(booking.getSeatId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 좌석이 존재하지 않습니다."));
+
+        if (seat.getSeatStatus() == SeatStatus.BOOKED || seat.getSeatStatus() == SeatStatus.OCCUPIED) {
             throw new IllegalStateException("좌석 예약 내역을 재확인 해주세요.");
         }
 
@@ -55,11 +62,12 @@ public class PaymentService {
          - 포인트 잔액이 결제할 가격보다 많아야 함.
          - 포인트 및 콘서트 정보 조회
         */
-        Point point = pointRepository.findById(paymentRequest.getUserId())
-                .orElseThrow(() -> new IllegalArgumentException("사용자 포인트 정보가 없습니다."));
-
+        //예약 요청한 콘서트의 가격 정보 필요
         Concert concertInfo = concertRepository.findById(booking.getConcert().getConcertId())
                 .orElseThrow(() -> new IllegalArgumentException("콘서트 정보가 없습니다."));
+
+        Point point = pointRepository.findById(paymentRequest.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("사용자 포인트 정보가 없습니다."));
 
         //포인트 부족 체크
         if (!point.isEnough(concertInfo.getPrice())) {
--- src/test/java/kr/hhplus/be/server/src/service/unit/PaymentServicetest.java
+++ src/test/java/kr/hhplus/be/server/src/service/unit/PaymentServicetest.java
@@ -1,14 +1,9 @@
 package kr.hhplus.be.server.src.service.unit;
 
 import jakarta.persistence.EntityNotFoundException;
-import kr.hhplus.be.server.src.domain.model.Booking;
-import kr.hhplus.be.server.src.domain.model.Concert;
-import kr.hhplus.be.server.src.domain.model.Point;
-import kr.hhplus.be.server.src.domain.model.User;
-import kr.hhplus.be.server.src.domain.repository.BookingRepository;
-import kr.hhplus.be.server.src.domain.repository.ConcertRepository;
-import kr.hhplus.be.server.src.domain.repository.PaymentRepository;
-import kr.hhplus.be.server.src.domain.repository.PointRepository;
+import kr.hhplus.be.server.src.domain.model.*;
+import kr.hhplus.be.server.src.domain.model.enums.SeatStatus;
+import kr.hhplus.be.server.src.domain.repository.*;
 import kr.hhplus.be.server.src.interfaces.payment.PaymentRequest;
 import kr.hhplus.be.server.src.service.PaymentService;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,7 +14,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PaymentServicetest {
@@ -39,10 +40,19 @@ class PaymentServicetest {
     @Mock
     private ConcertRepository concertRepository;
 
+    @Mock
+    private SeatRepository seatRepository;
+
+    private Concert mockConcert;
+    private User mockUser;
+    private Point mockPoint;
+    private Booking mockBooking;
+    private Seat mockSeat;
+
     @BeforeEach
     void setUp() {
 
-        Concert concert = Concert.builder()
+        mockConcert = Concert.builder()
                 .concertId(1L)
                 .name("BTS World Tour")
                 .price(150000L)
@@ -51,7 +61,7 @@ class PaymentServicetest {
                 .location("서울 올림픽 경기장")
                 .build();
 
-        User user = User.builder()
+        mockUser = User.builder()
                 .userId(1L)
                 .userName("김테스트")
                 .phoneNumber("010-1234-5678")
@@ -59,17 +69,22 @@ class PaymentServicetest {
                 .address("서울특별시 강서구 등촌동")
                 .build();
 
-        Point point = Point.builder()
-                .user(user)
+        mockPoint = Point.builder()
+                .user(mockUser)
                 .pointBalance(200000L)
                 .build();
 
-        Booking booking = Booking.builder()
+        mockBooking = Booking.builder()
                 .bookingId(1L)
-                .concert(concert)
+                .concert(mockConcert)
                 .seatNum(1L)
                 .seatId(1L)
-                .user(user)
+                .user(mockUser)
+                .build();
+
+        mockSeat = Seat.builder()
+                .seatId(mockBooking.getSeatId())
+                .seatStatus(SeatStatus.AVAILABLE) // 또는 SeatStatus.OCCUPIED
                 .build();
 
     }
@@ -88,7 +103,149 @@ class PaymentServicetest {
             paymentService.processPayment(paymentRequest);
         });
 
+    }
+
+    @DisplayName("결제 요청 유저와 예약된 유저가 동일하지 않으면 에러 발생한다.")
+    @Test
+    void paymentUserDiffBookedUserTest() {
+
+        //given
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setBookingId(1L);
+        paymentRequest.setUserId(99L);  //결제 요청 유저 id가 다름
+
+        given(bookingRepository.findById(1L)).willReturn(Optional.of(mockBooking));
+
+        // when & then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            paymentService.processPayment(paymentRequest);
+        });
+
+        assertEquals("유저 정보가 일치하지 않습니다.", exception.getMessage());
 
     }
 
+    @DisplayName("결제할 콘서트의 예약 좌석이 존재하지 않으면 실패한다.")
+    @Test
+    void bookingNoSeatErrorTest() {
+
+        //given
+        Long mockSeatId = 1L;
+
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setBookingId(1L);
+        paymentRequest.setUserId(1L);
+
+        when(bookingRepository.findById(1L)).thenReturn(Optional.of(mockBooking));
+        when(seatRepository.findById(mockSeatId)).thenReturn(Optional.empty());
+
+        //when & then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            paymentService.processPayment(paymentRequest);
+        });
+
+        assertEquals("해당 좌석이 존재하지 않습니다.", exception.getMessage());
+
+    }
+
+    @DisplayName("결제할 콘서트의 예약 좌석 점유 여부를 체크한다.")
+    @Test
+    void bookingOccupiedOrBookedSeatErrorTest() {
+        //given
+        Long mockSeatId = 1L;
+
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setBookingId(1L);
+        paymentRequest.setUserId(1L);
+
+        Seat mockSeat = Seat.builder()
+                .seatId(mockSeatId)
+                .seatStatus(SeatStatus.BOOKED) // 또는 SeatStatus.OCCUPIED
+                .build();
+
+        when(bookingRepository.findById(1L)).thenReturn(Optional.of(mockBooking));
+        when(seatRepository.findById(mockSeatId)).thenReturn(Optional.of(mockSeat));
+
+        //when & then
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> {
+            paymentService.processPayment(paymentRequest);
+        });
+
+        assertEquals("좌석 예약 내역을 재확인 해주세요.", exception.getMessage());
+    }
+
+    @DisplayName("결제할 콘서트의 가격 조회 시 정보 없으면 에러난다.")
+    @Test
+    void noConcertInfoErrorTest() {
+        //given
+        Long mockSeatId = 1L;
+
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setBookingId(1L);
+        paymentRequest.setUserId(1L);
+
+        when(bookingRepository.findById(1L)).thenReturn(Optional.of(mockBooking));
+        when(seatRepository.findById(mockSeatId)).thenReturn(Optional.of(mockSeat));
+        when(concertRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        // when & then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            paymentService.processPayment(paymentRequest);
+        });
+
+        assertEquals("콘서트 정보가 없습니다.", exception.getMessage());
+
+    }
+
+    @DisplayName("결제할 유저의 포인트 정보 조회 시 정보 없으면 에러난다.")
+    @Test
+    void noPointInfoErrorTest() {
+        //given
+        Long mockSeatId = 1L;
+
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setBookingId(1L);
+        paymentRequest.setUserId(1L);
+
+        when(bookingRepository.findById(1L)).thenReturn(Optional.of(mockBooking));
+        when(seatRepository.findById(mockSeatId)).thenReturn(Optional.of(mockSeat));
+        when(concertRepository.findById(anyLong())).thenReturn(Optional.ofNullable(mockConcert));
+        when(pointRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+
+        // when & then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            paymentService.processPayment(paymentRequest);
+        });
+
+        assertEquals("사용자 포인트 정보가 없습니다.", exception.getMessage());
+    }
+
+    @DisplayName("결제할 유저의 포인트가 부족하면 에러난다.")
+    @Test
+    void notEnoughPointTest() {
+        //given
+        Long mockSeatId = 1L;
+
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setBookingId(1L);
+        paymentRequest.setUserId(1L);
+
+        Point point = new Point();
+        point.setPointBalance(50000L);
+        point.setUserId(1L);
+
+        when(bookingRepository.findById(1L)).thenReturn(Optional.of(mockBooking));
+        when(seatRepository.findById(mockSeatId)).thenReturn(Optional.of(mockSeat));
+        when(concertRepository.findById(anyLong())).thenReturn(Optional.ofNullable(mockConcert));
+        when(pointRepository.findById(anyLong())).thenReturn(Optional.of(point));
+
+
+        // when & then
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> {
+            paymentService.processPayment(paymentRequest);
+        });
+
+        assertEquals("잔액을 확인해주세요.", exception.getMessage());
+    }
 }
