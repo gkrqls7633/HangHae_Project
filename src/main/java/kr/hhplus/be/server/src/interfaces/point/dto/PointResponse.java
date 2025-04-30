c6c69c512b48226f81118c25c850612156b13b4a
hakbinMac <gkrqls8409@gmail.com>
2025-04-30 17:47:58 +0900

    [6주차][심화] 포인트 조회 캐시 적용
     - 조회 시 캐시 조회
     - 충전 시 캐시만료

--- src/main/java/kr/hhplus/be/server/src/interfaces/point/dto/PointResponse.java
+++ src/main/java/kr/hhplus/be/server/src/interfaces/point/dto/PointResponse.java
@@ -1,13 +1,13 @@
 package kr.hhplus.be.server.src.interfaces.point.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PointResponse {
 
     @Schema(description = "포인트 잔액", example = "10000")
